0543d92cf639da1615e6578ada375f02817beb77
Mustaque Khan <130723242+mustaque789@users.noreply.github.com>
2024-02-10 16:19:36 -0500

    Update HomePage.jsx

--- src/customer/pages/HomePage/HomePage.jsx
+++ src/customer/pages/HomePage/HomePage.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import MainCarousel from '../../components/HomeCarousel/MainCarousel'
 
-
-// home page component
+// 
+// home
 const HomePage = () => {
   return (
     <div>
         <MainCarousel/>
 
         <div>
+          
             Other Sections
         </div>
       
