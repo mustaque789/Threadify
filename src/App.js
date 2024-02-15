0d63f06ca1ffa5bc33257f6d9aa668e37fb25b81
Mustaque Khan <130723242+mustaque789@users.noreply.github.com>
2024-02-14 23:30:41 -0500

    Add files via upload
    sprint 2

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-import logo from "./logo.svg";
-import "./App.css";
-import Navigation from "./customer/components/navigation/Navigation";
-import HomePage from "./customer/pages/HomePage/HomePage";
-
-// mern stack
+import logo from './logo.svg';
+import './App.css';
+import Navigation from './customer/components/Navigation/Navigation';
+import HomePage from './customer/pages/HomePage/HomePage';
+import Footer from './customer/components/Footer/Footer'
+import Product from './customer/components/Product/Product';
+import ProductDetails from './customer/components/ProductDetails/ProductDetails';
+import Cart from './customer/components/Cart/Cart';
+import Checkout from './customer/components/Checkout/Checkout';
 
 function App() {
   return (
-    <div className="App">
-      <Navigation />
-      <div>
-        <HomePage />
-      </div>
+    <div className="">
+      <Navigation/>
+        <div>
+        {/* <HomePage/> */}
+        {/* <Product/> */}
+        {/* <ProductDetails/> */}
+        <Cart/>
+        {/* <Checkout/> */}
+        </div>  
+        <Footer/>
     </div>
   );
 }
