02481cb1b0f9f3fa093a34960ac0e5460b03b006
Mustaque Khan <130723242+mustaque789@users.noreply.github.com>
2024-02-20 02:04:02 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -16,6 +16,6 @@ root.render(
 );
 
 // If you want to start measuring performance in your app, pass a function
-// to log results
+
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
