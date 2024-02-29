59b2fc562f45891a5019f99ae96442f52dcce0eb
Mustaque Khan <130723242+mustaque789@users.noreply.github.com>
2024-02-29 10:13:20 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Product from './customer/components/Product/Product';
 import ProductDetails from './customer/components/ProductDetails/ProductDetails';
 import Cart from './customer/components/Cart/Cart';
 import Checkout from './customer/components/Checkout/Checkout';
-// product checkout navigation implemented
+// product checkout navigation 
 function App() {
   return (
     <div className="">
