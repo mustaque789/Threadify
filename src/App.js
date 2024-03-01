453ca388f5394419bd3b1ef20da1da23d10bbc01
Mustaque Khan <130723242+mustaque789@users.noreply.github.com>
2024-03-01 10:04:17 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Product from './customer/components/Product/Product';
 import ProductDetails from './customer/components/ProductDetails/ProductDetails';
 import Cart from './customer/components/Cart/Cart';
 import Checkout from './customer/components/Checkout/Checkout';
-// product te
+// product template
 function App() {
   return (
     <div className="">
