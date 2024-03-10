5b1a69fec502182955ffddabf5fde93f33be336f
Mustaque Khan <130723242+mustaque789@users.noreply.github.com>
2024-03-09 23:44:11 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Cart from './customer/components/Cart/Cart';
 import Checkout from './customer/components/Checkout/Checkout';
 // component routing
 function App() {
-// all components routed in 
+// all components routed 
   return (
     <div className="">
       <Navigation/>
