06fdddb046583712da08cc3310159436643ea21c
Mustaque Khan <130723242+mustaque789@users.noreply.github.com>
2024-04-24 00:44:37 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Product from './customer/components/Product/Product';
 import ProductDetails from './customer/components/ProductDetails/ProductDetails';
 import Cart from './customer/components/Cart/Cart';
 import Checkout from './customer/components/Checkout/Checkout';
-// component structure  
+// component structure file
 function App() {
 // routed components file
   return (
